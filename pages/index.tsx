e55a8b760c614ae7ef8f6bf5df76bf023ee24655
Jorge Serrano <jorge.serrano@mercadolibre.com>
2021-11-25 20:40:56 -0300

    add support for scss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import type { NextPage } from 'next'
 import Layout from '../components/Layout'
 import ParticlesContainer from '../components/ParticlesContainer'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return <Layout>
     <div className={styles.cajita}>
-
       <div>
         <h1 className={styles.title}>JORGE SERRANO</h1>
         <h2 className={styles.title}>Upcoming</h2>
