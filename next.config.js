bfd1820097e2d2f700123650cff93270ec4cd695
Jorge Serrano <entrevistasmias@gmail.com>
2023-04-03 20:44:51 -0300

    Some changes of  (#5)
    * base configs@
    
    * Delete .DS_Store
    
    * add meta data

--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
+const { resolve, join } = require('path');
 
 module.exports = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  }
-}
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [
+            join(__dirname, 'styles'),
+            join(__dirname, 'components/*/*.scss'),
+        ],
+    },
+    webpack: {
+        resolve: {
+            alias: {
+                '@components': resolve(__dirname, 'components'),
+                '@config': resolve(__dirname, 'config'),
+                '@hooks': resolve(__dirname, 'hooks'),
+                '@utils': resolve(__dirname, 'utils'),
+            },
+        },
+    },
+};
