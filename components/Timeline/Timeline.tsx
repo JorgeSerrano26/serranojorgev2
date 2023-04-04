7b762825cf15e60c07f332aed3bbb486558472a8
Jorge Serrano <entrevistasmias@gmail.com>
2023-04-04 16:32:00 -0300

    add timeline

--- components/Timeline/Timeline.tsx
+++ components/Timeline/Timeline.tsx
@@ -20,12 +20,12 @@ const SchoolIcon = () => (
 
 const Timeline = () => {
     return (
-        <VerticalTimeline lineColor="#f2f2f2">
+        <VerticalTimeline lineColor="#f2f2f2" layout="1-column-left">
             <VerticalTimelineElement
                 className="vertical-timeline-element--work"
                 contentStyle={{
                     background: '#212529',
-                    color: '#fff',
+                    color: '#f2f2f2',
                 }}
                 contentArrowStyle={{
                     borderRight: '7px solid #212529',
@@ -53,6 +53,10 @@ const Timeline = () => {
             <VerticalTimelineElement
                 className="vertical-timeline-element--work"
                 date="2010 - 2011"
+                contentStyle={{
+                    background: '#212529',
+                    color: '#f2f2f2',
+                }}
                 iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                 icon={<WorkIcon />}
             >
@@ -72,6 +76,10 @@ const Timeline = () => {
                 date="2008 - 2010"
                 iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                 icon={<WorkIcon />}
+                contentStyle={{
+                    background: '#212529',
+                    color: '#f2f2f2',
+                }}
             >
                 <h3 className="vertical-timeline-element-title">
                     Web Designer
@@ -86,6 +94,10 @@ const Timeline = () => {
                 date="2006 - 2008"
                 iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                 icon={<WorkIcon />}
+                contentStyle={{
+                    background: '#212529',
+                    color: '#f2f2f2',
+                }}
             >
                 <h3 className="vertical-timeline-element-title">
                     Web Designer
@@ -95,53 +107,6 @@ const Timeline = () => {
                 </h4>
                 <p>User Experience, Visual Design</p>
             </VerticalTimelineElement>
-            <VerticalTimelineElement
-                className="vertical-timeline-element--education"
-                date="April 2013"
-                iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                icon={<SchoolIcon />}
-            >
-                <h3 className="vertical-timeline-element-title">
-                    Content Marketing for Web, Mobile and Social Media
-                </h3>
-                <h4 className="vertical-timeline-element-subtitle">
-                    Online Course
-                </h4>
-                <p>Strategy, Social Media</p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-                className="vertical-timeline-element--education"
-                date="November 2012"
-                iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                icon={<SchoolIcon />}
-            >
-                <h3 className="vertical-timeline-element-title">
-                    Agile Development Scrum Master
-                </h3>
-                <h4 className="vertical-timeline-element-subtitle">
-                    Certification
-                </h4>
-                <p>Creative Direction, User Experience, Visual Design</p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-                className="vertical-timeline-element--education"
-                date="2002 - 2006"
-                iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                icon={<SchoolIcon />}
-            >
-                <h3 className="vertical-timeline-element-title">
-                    Bachelor of Science in Interactive Digital Media Visual
-                    Imaging
-                </h3>
-                <h4 className="vertical-timeline-element-subtitle">
-                    Bachelor Degree
-                </h4>
-                <p>Creative Direction, Visual Design</p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-                iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-                icon={<StarIcon />}
-            />
         </VerticalTimeline>
     );
 };
