77af653349e9975b2bac7a70cf3b50afa0c866cd
Jorge Serrano <jorge.serrano@mercadolibre.com>
2023-06-05 12:43:46 -0300

    add changes

--- pages/tests/index.tsx
+++ pages/tests/index.tsx
@@ -1,15 +1,10 @@
 import type { NextPage } from 'next';
 import Layout from '@components/Layout';
-import Timeline, { TimelineProps } from '@components/Timeline/Timeline';
-import data from './data.json';
-
-const list = data as TimelineProps['list'];
 
 const Home: NextPage = () => {
     return (
         <Layout>
             <div>
-                <Timeline list={list} />
             </div>
         </Layout>
     );
