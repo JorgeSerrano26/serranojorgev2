6d6d9c7a144c9ed0bd816ec132a401f1255b4031
Jorge Serrano <entrevistasmias@gmail.com>
2023-07-19 19:05:06 -0300

    remove component

--- app/maintenance.tsx
+++ app/maintenance.tsx
@@ -1,13 +1,7 @@
-import type { NextPage } from 'next';
-import Layout from '@components/Layout';
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-    return (
-        <Layout>
-            
-        </Layout>
-    );
+	return <main>hola</main>;
 };
-
 
 export default Home;
