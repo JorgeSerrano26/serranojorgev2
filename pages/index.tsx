c71cbffa39f5a47fde2bacc2066e28ed6549d0e0
Jorge Serrano <entrevistasmias@gmail.com>
2023-07-19 14:51:01 -0300

    Change linter
    Remove eslint/prettier and add rome

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,63 @@
-import type { GetServerSideProps, NextPage } from 'next';
-import Layout from '@components/Layout';
-import styles from '@styles/Home.module.scss';
-import Image from 'next/image';
-import { Badge } from '@nextui-org/react';
-import ScrollIcon from '@components/ScrollIcon/ScrollIcon';
-import ExperienceService, { Experience, Project } from 'services/ExperienceService';
-import FloatingMenu from '@components/FloatingMenu/FloatingMenu';
+import FloatingMenu from "@components/FloatingMenu/FloatingMenu";
+import Layout from "@components/Layout";
+import ScrollIcon from "@components/ScrollIcon/ScrollIcon";
+import { Badge } from "@nextui-org/react";
+import styles from "@styles/Home.module.scss";
+import type { GetServerSideProps, NextPage } from "next";
+import Image from "next/image";
+import ExperienceService, {
+	Experience,
+	Project,
+} from "services/ExperienceService";
 
 type Props = {
-    experience: Experience[];
-    projects: Project[];
-}
+	experience: Experience[];
+	projects: Project[];
+};
 
 const Home: NextPage<Props> = ({ projects }) => {
-    return (
-        <Layout>
-            <FloatingMenu items={[
-                { href: "https://github.com/JorgeSerrano26", icon: "github", ariaLabel: "jorge github" },
-                { href: "twitch.tv/jorgeserrano", icon: "twitch", ariaLabel: "jorge twitch", disabled: true },
-                { href: "https://www.linkedin.com/in/jorge-serrano-a6b627162/", icon: "linkedin", ariaLabel: "jorge linkedin" }
-            ]} />
-            <div className={styles.cajita}>
-                <div>
-                    <h1 className={`${styles.title} text-6xl font-semibold mb-2`}>JORGE SERRANO</h1>
-                    <Badge color="primary" variant="flat" disableOutline enableShadow>
-                        UNDER DEVELOPING
-                    </Badge>
-                </div>
-                <ScrollIcon className={styles.scroll_icon_home} />
-            </div>
-            <div className="relative hidden md:block h-[1000px]">
-                <div className="absolute left-0 top-[-400px] z-[0]">
-                    <Image src="/home-purple.svg" priority width={271} height={688} />
-                </div>
-                <div className=" absolute right-0 z-[-1]">
-                    <Image src="/home-green.svg" width={271} priority height={688} />
-                </div>
-            </div>
-            {/* <section id="projects" className='pt-8 pb-8'>
+	return (
+		<Layout>
+			<FloatingMenu
+				items={[
+					{
+						href: "https://github.com/JorgeSerrano26",
+						icon: "github",
+						ariaLabel: "jorge github",
+					},
+					{
+						href: "twitch.tv/jorgeserrano",
+						icon: "twitch",
+						ariaLabel: "jorge twitch",
+						disabled: true,
+					},
+					{
+						href: "https://www.linkedin.com/in/jorge-serrano-a6b627162/",
+						icon: "linkedin",
+						ariaLabel: "jorge linkedin",
+					},
+				]}
+			/>
+			<div className={styles.cajita}>
+				<div>
+					<h1 className={`${styles.title} text-6xl font-semibold mb-2`}>
+						JORGE SERRANO
+					</h1>
+					<Badge color="primary" variant="flat" disableOutline enableShadow>
+						UNDER DEVELOPING
+					</Badge>
+				</div>
+				<ScrollIcon className={styles.scroll_icon_home} />
+			</div>
+			<div className="relative hidden md:block h-[1000px]">
+				<div className="absolute left-0 top-[-400px] z-[0]">
+					<Image src="/home-purple.svg" priority width={271} height={688} />
+				</div>
+				<div className=" absolute right-0 z-[-1]">
+					<Image src="/home-green.svg" width={271} priority height={688} />
+				</div>
+			</div>
+			{/* <section id="projects" className='pt-8 pb-8'>
                 <Container>
                     <h2 className="text-5xl font-semibold text-center mt-3 mb-3">PROJECTS</h2>
                     <Grid.Container gap={2} justify="center">
@@ -51,21 +71,24 @@ const Home: NextPage<Props> = ({ projects }) => {
                     </Grid.Container>
                 </Container>
             </section> */}
-        </Layout>
-    );
+		</Layout>
+	);
 };
 
-export const getServerSideProps: GetServerSideProps<Props, any, any> = async (context) => {
-    const [experience, projects] = await Promise.all([
-        ExperienceService.getExperience(),
-        ExperienceService.getProjects()]);
+export const getServerSideProps: GetServerSideProps<Props, {}, {}> = async (
+	context,
+) => {
+	const [experience, projects] = await Promise.all([
+		ExperienceService.getExperience(),
+		ExperienceService.getProjects(),
+	]);
 
-    return {
-        props: {
-            experience,
-            projects
-        }
-    }
-}
+	return {
+		props: {
+			experience,
+			projects,
+		},
+	};
+};
 
 export default Home;
