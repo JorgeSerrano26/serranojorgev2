3f04bec05bca573a8351492c84bac667fbb1745f
Jorge Serrano <entrevistasmias@gmail.com>
2023-07-21 21:26:18 -0300

    add z index

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ const Home: NextPage = async () => {
 				className="w-[70%] md:w-60% max-w-screen-md m-auto pt-20 pb-20"
 			>
 				<div className="text-center">
-					<h1 className="inline-block z-1 relative text-4xl font-semibold mb-10 after:content-[''] after:w-full after:h-3 after:block after:absolute after:bg-fuchsia-500 after:top-[75%] after:z-[-1]">
+					<h1 className="inline-block z-10 relative text-4xl font-semibold mb-10 after:content-[''] after:w-full after:h-3 after:block after:absolute after:bg-fuchsia-500 after:top-[75%] after:z-[-1]">
 						Experience
 					</h1>
 				</div>
