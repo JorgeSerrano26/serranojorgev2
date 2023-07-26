61b96208abbf7c2dd4382c1e8df6030102248d25
Jorge Serrano <entrevistasmias@gmail.com>
2023-07-26 20:28:06 -0300

    Add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Alert from "@components/Alert";
 import FloatingMenu, {
 	type FloatingMenuProps,
 } from "@components/FloatingMenu/FloatingMenu";
@@ -22,21 +23,37 @@ const Home: NextPage = async () => {
 	return (
 		<main>
 			<FloatingMenu items={floatingsItems as FloatingMenuProps["items"]} />
-			<div className="flex flex-row relative top-0 left-0 justify-center items-center w-100 h-screen text-center">
-				<div>
-					<h1 className="text-6xl font-semibold mb-2">JORGE SERRANO</h1>
-					<span className="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
-						UNDER DEVELOPING
-					</span>
-					<div className="mt-10">
-						<a
-							href="/jorge_serrano_cv.pdf"
-							className="animate-bounce text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-							download
-						>
-							<IconDownload className="mr-2" />
-							Download cv
-						</a>
+			<div id="home" className="top-0 left-0 w-100 h-screen pt-20">
+				<div className="container m-auto grid grid-rows-3 md:grid-rows-1 grid-cols-1 md:grid-cols-2 place-items-center ">
+					{/* INFO */}
+					<div className="grid grid-rows-2 md:grid-rows-3 col-start-1">
+						<div className="place row-start-1 row-end-4">
+							<h1 className="text-6xl font-semibold mb-2 d-inline text-center">
+								JORGE SERRANO
+							</h1>
+							<Alert />
+							<div className="flex justify-center mt-10">
+								<a
+									href="/jorge_serrano_cv.pdf"
+									className="animate-bounce text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+									download
+								>
+									<IconDownload className="mr-2" />
+									Download cv
+								</a>
+							</div>
+						</div>
+					</div>
+					{/* IMAGE */}
+					<div className="flex justify-center relative md:col-start-2 row-start-2 row-end-4 md:row-start-1 md:row-end-1">
+						<img
+							src="/me.webp"
+							width={698}
+							height={766}
+							className="w-[70%] md:w-full"
+							alt="Jorge Serrano"
+							fetchPriority="high"
+						/>
 					</div>
 				</div>
 				<ScrollIcon className="top-[99%] translate-y-[-50%]" />
@@ -93,7 +110,7 @@ const Home: NextPage = async () => {
 											width={50}
 											height={50}
 										/>
-										<div className="absolute top-[110%] z-10 shadow transform-translate-x-1/2 px-2 py-1 bg-gray-50 text-gray-800 text-sm rounded opacity-0 group-hover:opacity-100">
+										<div className="absolute top-[110%] w-max z-10 shadow transform-translate-x-1/2 px-2 py-1 bg-gray-50 text-gray-800 text-sm rounded opacity-0 group-hover:opacity-100">
 											{tech.name}
 										</div>
 									</div>
@@ -123,7 +140,7 @@ const Home: NextPage = async () => {
 											width={50}
 											height={50}
 										/>
-										<div className="absolute top-[110%] z-10 shadow transform-translate-x-1/2 px-2 py-1 bg-gray-50 text-gray-800 text-sm rounded opacity-0 group-hover:opacity-100">
+										<div className="absolute top-[110%] w-max z-10 shadow transform-translate-x-1/2 px-2 py-1 bg-gray-50 text-gray-800 text-sm rounded opacity-0 group-hover:opacity-100">
 											{tech.name}
 										</div>
 									</div>
---
+++ components/Alert.tsx
@@ -0,0 +1,19 @@
+const Alert = () => {
+	return (
+		<div
+			id="alert-4"
+			className="flex items-center p-4 mb-4 text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300"
+			role="alert"
+		>
+			<span role="img" className="animate-ping">
+				🚨
+			</span>
+			<div className="ml-3 text-sm font-medium">
+				This page is under construction
+			</div>
+			<span className="sr-only">Info</span>
+		</div>
+	);
+};
+
+export default Alert;
