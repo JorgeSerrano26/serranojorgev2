c3e6410a38915d394c7f7124da665bccd4efe2ae
Jorge Serrano <entrevistasmias@gmail.com>
2023-12-27 15:18:06 -0300

    New background

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,9 @@ export default function RootLayout({
 					sizes="180x180"
 				/>
 			</Head>
-			<body className="relative">
+			<body className="relative h-full w-full bg-slate-950 overflow-x-hidden">
+				<div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]" />
+				<div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]" />
 				{children}
 				<Footer />
 				<ScrollToTop />
