d6efab8ad45589389df1411effcfc075fe6888b1
Jorge Serrano <entrevistasmias@gmail.com>
2024-03-26 16:06:14 -0300

    Add robots and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
+import sitemap from "@astrojs/sitemap";
 import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
+import robotsTxt from "astro-robots-txt";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), robotsTxt(), sitemap()],
   compilerOptions: {
     "plugins": [{
       "name": "@astrojs/ts-plugin"
     }]
   },
+  site: "https://jorgeserrano.com.ar",
   output: "server",
   adapter: vercel()
 });
